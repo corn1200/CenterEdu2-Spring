eee1cf02a3c58b65b2dcdc296cf0ee4f244c326f
corn1200 <corn120033@gmail.com>
2020-12-14 14:12:46 +0900

    add: repository 에 UserRepository 를 추가하였습니다.

---
+++ src/main/java/com/kimhs/apis/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.kimhs.apis.repository;
+
+import com.kimhs.apis.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
