c27416beae90761380569b453a6b8f57374850e0
corn1200 <corn120033@gmail.com>
2020-12-16 11:46:00 +0900

    feat: 모든 상품을 조회하는 기능을 추가한다.

--- src/main/java/com/kimhs/apis/route/ProductRoute.java
+++ src/main/java/com/kimhs/apis/route/ProductRoute.java
@@ -8,6 +8,8 @@ import com.kimhs.apis.vo.UserRegisterVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/product")
 public class ProductRoute {
@@ -17,6 +19,10 @@ public class ProductRoute {
     public ProductRoute(ProductService productService) {
         this.productService = productService;
     }
+
+    @GetMapping("")
+    @ResponseBody
+    public List<User> getProducts() { return this.productService.findAll(); }
 
     @GetMapping("/{product_id}")
     @ResponseBody
