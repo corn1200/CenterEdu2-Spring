ed224d971c7989a1db155ae67bf89fc0b5d49bcf
corn1200 <corn120033@gmail.com>
2020-12-17 14:22:27 +0900

    feat: product 를 추가할 때 imageUrl 을 받도록 한다.

--- src/main/java/com/kimhs/apis/vo/ProductRegisterVO.java
+++ src/main/java/com/kimhs/apis/vo/ProductRegisterVO.java
@@ -13,12 +13,13 @@ public class ProductRegisterVO {
     private int listPrice;
     private int price;
     private String category;
+    private String imageUrl;
 
     @Override
     public String toString() {
         return String.format(
-                "ProductRegisterVO[name='%s', description='%s', listPrice='%s', price='%s']",
-                this.name, this.description, this.listPrice, this.price
+                "ProductRegisterVO[name='%s', description='%s', listPrice='%s', price='%s', category='%s', imageUrl='%s']",
+                this.name, this.description, this.listPrice, this.price, this.category, this.imageUrl
         );
     }
 }
