1859cb1f1666fbd6d6b8318a280dd7e286637282
corn1200 <corn120033@gmail.com>
2020-12-18 14:43:55 +0900

    delete: 불필요한 Route 파일을 삭제한다

--- src/main/java/com/kimhs/apis/route/IssuedCouponRoute.java
+++
@@ -1,9 +0,0 @@
-package com.kimhs.apis.route;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("")
-public class IssuedCouponRoute {
-}
