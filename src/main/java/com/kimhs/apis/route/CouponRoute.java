9f3d39b95651a7c4c283476d5b27cafbac8f5f01
corn1200 <corn120033@gmail.com>
2020-12-18 14:44:31 +0900

    feat: 쿠폰을 발급하는 경로와 발급한 쿠폰을 확인하는 경로를 추가한다

--- src/main/java/com/kimhs/apis/route/CouponRoute.java
+++ src/main/java/com/kimhs/apis/route/CouponRoute.java
@@ -1,7 +1,9 @@
 package com.kimhs.apis.route;
 
 import com.kimhs.apis.model.Coupon;
+import com.kimhs.apis.model.IssuedCoupon;
 import com.kimhs.apis.service.CouponService;
+import com.kimhs.apis.service.IssuedCouponService;
 import com.kimhs.apis.vo.CouponRegisterVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,10 +12,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/coupon")
 public class CouponRoute {
     private final CouponService couponService;
+    private final IssuedCouponService issuedCouponService;
 
     @Autowired
-    public CouponRoute(CouponService couponService) {
+    public CouponRoute(CouponService couponService,
+                       IssuedCouponService issuedCouponService) {
         this.couponService = couponService;
+        this.issuedCouponService = issuedCouponService;
     }
 
     @GetMapping("/{coupon_id}")
@@ -25,5 +30,17 @@ public class CouponRoute {
     @PostMapping
     public int createCoupon(CouponRegisterVO couponRegisterVO) throws Exception {
         return this.couponService.createCoupon(couponRegisterVO);
+    }
+
+    @PostMapping("/{coupon_id}/issue")
+    public int issueCoupon(@PathVariable(value = "coupon_id") String couponId,
+                           @RequestParam(value = "user_id") String userId) throws Exception {
+        return this.issuedCouponService.issueCoupon(Integer.parseInt(couponId), Integer.parseInt(userId));
+    }
+
+    @GetMapping("/issued-coupon/{issued_coupon_id}")
+    @ResponseBody
+    public IssuedCoupon getIssuedCoupon(@PathVariable(value = "issued_coupon_id") String issuedCouponId) throws Exception {
+        return this.issuedCouponService.issueCouponById(Integer.parseInt(issuedCouponId));
     }
 }
